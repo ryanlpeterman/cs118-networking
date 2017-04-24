5d0dc2ff520fd9cadbf9d1b1f8c4f3074df63c59
Ryan Peterman <RyanLPeterman@gmail.com>
2017-04-24 00:47:24 -0700

    add dynamic host resolution

--- client.cpp
+++ client.cpp
@@ -7,6 +7,8 @@
 #include <stdio.h>
 #include <errno.h>
 #include <unistd.h>
+#include <netinet/in.h>
+#include <netdb.h>
 
 #include <iostream>
 #include <vector>
@@ -44,9 +46,14 @@ int main(int argc, char* argv[])
 	}
 
 	// save arguments
-    std::string hostnameOrIP(argv[1]);
+	std::string fileName(argv[3]);
     short port = std::atoi(argv[2]);
-    std::string fileName(argv[3]);
+    const int MIN_NON_PRIV_PORT = 1024;
+	const int MAX_PORT = 65535;
+    if (port < MIN_NON_PRIV_PORT || port > MAX_PORT) {
+    	std::cerr << "ERROR: invalid port/port out of range error" << std::endl;
+    	exit(1);
+    }
 
     // check if file exists
     if (!is_regular_file(argv[3])) {
@@ -54,43 +61,39 @@ int main(int argc, char* argv[])
     	exit(1);
     }
 
+ 	int status;
+	struct addrinfo hints;
+	struct addrinfo *res;  // will point to the results
+
+	memset(&hints, 0, sizeof hints); // make sure the struct is empty
+	hints.ai_family = AF_UNSPEC;     // don't care IPv4 or IPv6
+	hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+	// hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
+
+	// getaddrinfo(hostnameip, port, input datastruct, outparam)
+	if ((status = getaddrinfo(argv[1], argv[2], &hints, &res)) != 0) {
+	    std::cerr << "ERROR: getaddrinfo error" << std::endl;
+	    exit(1);
+	}
+
+	// if resolving addr resulted in no ip
+	if(res == NULL) {
+		std::cerr << "ERROR: could not resolve given domain/ip" << std::endl;
+		exit(1);
+	}
+
  	// create a socket using TCP IP
- 	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-
- 	// struct sockaddr_in addr;
- 	// addr.sin_family = AF_INET;
- 	// addr.sin_port = htons(40001);     // short, network byte order
- 	// addr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 	// memset(addr.sin_zero, '\0', sizeof(addr.sin_zero));
- 	// if (bind(sockfd, (struct sockaddr *)&addr, sizeof(addr)) == -1) {
- 	//   perror("bind");
- 	//   return 1;
- 	// }
-
- 	struct sockaddr_in serverAddr;
- 	serverAddr.sin_family = AF_INET;
- 	serverAddr.sin_port = htons(port);     // short, network byte order
- 	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
- 	memset(serverAddr.sin_zero, '\0', sizeof(serverAddr.sin_zero));
+ 	int sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
 
 	// connect to the server
-	if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) == -1) {
+	if (connect(sockfd, res->ai_addr, res->ai_addrlen) == -1) {
 		perror("connect");
 		return 2;
 	}
 
-	struct sockaddr_in clientAddr;
-	socklen_t clientAddrLen = sizeof(clientAddr);
-	if (getsockname(sockfd, (struct sockaddr *)&clientAddr, &clientAddrLen) == -1) {
-		perror("getsockname");
-		return 3;
-	}
-
 	char ipstr[INET_ADDRSTRLEN] = {'\0'};
-	inet_ntop(clientAddr.sin_family, &clientAddr.sin_addr, ipstr, sizeof(ipstr));
-	std::cout << "Set up a connection from: " << ipstr << ":" <<
-	ntohs(clientAddr.sin_port) << std::endl;
-
+	inet_ntop(res->ai_family, &(res->ai_addr), ipstr, sizeof ipstr);
+	std::cout << "Set up a connection from: " << ipstr << ":" << port << std::endl;
 
 	std::string file_contents = read_file(fileName);
 	size_t bytes_written = 0;
@@ -109,6 +112,7 @@ int main(int argc, char* argv[])
 
  	std::cout << "Transfer complete! Total number of bytes written: " << bytes_written << std::endl;
 
+ 	freeaddrinfo(res); // free the linked-list
   	close(sockfd);
   	return 0;
 }
